362f69b8cecf7e663b057a1accee99a44d29c1cc
Raulj06 <5015@holbertonschool.com>
2022-09-08 23:57:33 +0000

    alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import string
 for i in range(ord('a'), ord('z')+1):
-o    if i == ord('e') or i == ord('q'):
+    if i == ord('e') or i == ord('q'):
     continue
     print("{:c}".format(i), end="")
