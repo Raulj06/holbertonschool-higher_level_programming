be61ee2a7627dafbd6db4b76001efe82060b6cba
Raulj06 <5015@holbertonschool.com>
2022-09-13 22:08:48 +0000

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    s = "".join(x for x in my_string if x != 'c' if x != 'C')
-    return s
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        print(" ".join("{:d}".format(var) for var in i))
