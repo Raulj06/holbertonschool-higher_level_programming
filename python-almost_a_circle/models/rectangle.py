234bbb10650810e1f800f66689f1302f66917978
Raulj06 <5015@holbertonschool.com>
2022-10-08 03:32:16 +0000

    fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,111 +1,149 @@
 #!/usr/bin/python3
-"""Rectangle INIT"""
-
+""" Module class Rectangle """
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """creates rectangle class and inits it with values"""
+    """ Class Rectangle Defined """
+    __width = None
+    __height = None
+    __x = None
+    __y = None
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """attributes for rectangle class"""
-        self.width = width
-        self.height = height
+        """ Docstring of __init__ method
+        Args:
+            width (int): integer representing width of rectangle
+            height (int): integer representing height of rectangle
+            x (int): integer representing a coordinate
+            y (int): integer representing a cooordinate
+            id (int): integer representing an identification number
+        """
         self.x = x
         self.y = y
         super().__init__(id)
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """getter method width"""
+        """ width getter """
         return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter
+        Args:
+            value (int): Contains whatever was inside of width """
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        if (value <= 0):
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
-        """Height getter method"""
+        """ height getter """
         return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter
+        Args:
+            value (int): Contains whatever was inside of height """
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        if (value <= 0):
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
-        """x getter method"""
+        """ x coordinate getter """
         return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x coordinate setter
+        Args:
+            value (int): Contains whatever was inside of x """
+        if (type(value) != int):
+            raise TypeError("x must be an integer")
+        if (value < 0):
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
-        """y getter method"""
+        """ y coordinate getter """
         return self.__y
 
-    @width.setter
-    def width(self, width_val):
-        """width setter method"""
-        if type(width_val) is not int:
-            raise TypeError("width must be an integer")
-        if width_val <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = width_val
-
-    @height.setter
-    def height(self, height_val):
-        """height setter method"""
-        if type(height_val) is not int:
-            raise TypeError("height must be an integer")
-        if height_val <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = height_val
-
-    @x.setter
-    def x(self, x_val):
-        """x setter method"""
-        if type(x_val) is not int:
-            raise TypeError("x must be an integer")
-        if x_val < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = x_val
-
     @y.setter
-    def y(self, y_val):
-        """y setter method"""
-        if type(y_val) is not int:
+    def y(self, value):
+        """ y coordinate setter
+        Args:
+            value (int): Contains whatever was inside of y """
+        if (type(value) != int):
             raise TypeError("y must be an integer")
-        if y_val < 0:
+        if (value < 0):
             raise ValueError("y must be >= 0")
-        self.__y = y_val
+        self.__y = value
 
     def area(self):
-        """area of rectangle"""
-        return self.width * self.height
+        """ Docstring of area of the rectangle """
+        return self.__width * self.__height
 
     def display(self):
-        """prints out rectangle"""
-        for w_idx in range(self.y):
-            print('')
-        for idx in range(self.height):
-            for h_idx in range(self.x):
-                print(" ", end="")
-            for hashtag in range(self.width):
-                print('#', end='')
-            print('')
+        """ Docstring of the display of the rectangle based on
+        width and height """
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            print("")
+        for x in range(self.__height):
+            if self.__y != 0 and x == 0:
+                for cor_y in range(self.__y):
+                    string += '\n'
+            if self.__x != 0:
+                for cor_x in range(self.__x):
+                    string += " "
+            string += '#'
+            for y in range(self.__width - 1):
+                x *= y
+                string += '#'
+            string += '\n'
+        print(string[:-1])
 
     def __str__(self):
+        """ Docstring __str__ string """
         return "[Rectangle] ({}) {}/{} - {}/{}"\
-                .format(self.id, self.x, self.y, self.width, self.height)
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
-        """updates a varying number of attributes"""
-        if len(args) != 0:
-            atts = ["id", "width", "height", "x", "y"]
-            for idx, att, in enumerate(args):
-                setattr(self, atts, att)
+        """ Docstring update, where attributes and dictionaries are actualized
+        Args:
+            *args (pointer to array): Contains all arguments passed by multiple
+            parameter transfer
+            **kwargs (double pointer to dictionary): If *args does not exist
+            then use this to create a dictionary
+        """
+        attributes = ["id", "width", "height", "x", "y"]
+        if len(args) > 0:
+            count = 0
+            for arg in args:
+                setattr(self, attributes[count], arg)
+                count += 1
         else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            for key, values in kwargs.items():
+                setattr(self, key, values)
 
     def to_dictionary(self):
-        """dict representation"""
-        the_dict = {}
-        the_dict["id"] = self.id
-        the_dict["width"] = self.width
-        the_dict["height"] = self.height
-        the_dict["x"] = self.x
-        the_dict["y"] = self.y
-        return the_dict
+        """ Docstring to_dictionary method that returns the
+        dictionary representation of a Rectangle """
+        return {
+                "x": self.__x,
+                "y": self.__y,
+                "id": self.id,
+                "height": self.height,
+                "width": self.width
+                }
