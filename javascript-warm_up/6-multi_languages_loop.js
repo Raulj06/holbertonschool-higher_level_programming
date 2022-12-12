73e5d20b349d255ea78af6156e234bc86fc8c64e
Raulj06 <5015@holbertonschool.com>
2022-12-12 23:25:51 +0000

    task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Print 3 lines using an array of strings and a loop
+
+const lst = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const length = lst.length;
+let i = 0;
+for (i = 0; i < length; i++) {
+  console.log(lst[i]);
+}
