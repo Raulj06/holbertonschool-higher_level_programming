af637636e4e6f201e1954946b916fa5efba6da4d
Raulj06 <5015@holbertonschool.com>
2022-12-12 22:26:44 +0000

    task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that prints a message depending of the arguments passed
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+}
+if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
+}
