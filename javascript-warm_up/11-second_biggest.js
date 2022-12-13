735d4a2bfb68cb190bc04e3d1513fefb98f2cca7
Raulj06 <5015@holbertonschool.com>
2022-12-13 02:25:14 +0000

    task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Script that searvhes the second biggest int in list
+
+const { argv } = require('process');
+
+const argc = argv.length;
+const arr = [];
+
+if (argc < 4) {
+  console.log(0);
+} else {
+  for (let index = 2; index < argc; index++) {
+    arr.push(parseInt(argv[index], 10));
+  }
+  arr.sort(function (a, b) { return a - b; });
+  console.log(arr[arr.length - 2]);
+}
